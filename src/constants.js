84502241755c3a78ca9aa89b6a30afc2c835f7f5
mrkarezina <mrk.arezina@gmail.com>
2019-12-23 22:46:13 -0500

    Fix sendgrid key

--- src/constants.js
+++ src/constants.js
@@ -9,8 +9,7 @@ const DB_ID = '1';
 const PROCESSOR_ID = 'language-processor-health';
 const WYZEFIND_CORE_API_BASE = 'https://wyzefind-api-dot-graph-intelligence.appspot.com';
 const WYZEFIND_RSS_URL = 'https://us-central1-media-voice-applications.cloudfunctions.net/fetch_rss';
-const SENDGRID_API_KEY = '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';
-
+const SENDGRID_API_KEY = require('sendgrid-api-key.json').key;
 
 module.exports = {
     NUMBER_OF_ARTICLES,
